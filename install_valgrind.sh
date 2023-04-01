4f992c1db6efe7b6e7bcab3e5f88d354e79641b2
david.abou@epitech.eu <david.abou@epitech.eu>
2023-04-01 18:11:54 -0400

    Update valgind installer structure and durability

--- install_valgrind.sh
+++ install_valgrind.sh
@@ -1,34 +1,71 @@
 #!/bin/bash
+
+# Variables
 URL="https://sourceware.org/pub/valgrind"
 TARBALL="valgrind-3.20.0.tar.bz2"
 DIR="valgrind-3.20.0"
-SUDO="/usr/bin/sudo"
+TMP_DIR="/tmp"
+DEPENDENCIES=("wget" "tar" "gcc" "make")
 
-if [ $UID -eq "0" ]; then
-    SUDO=""
-    echo "[no sudo for root]"
-fi
+# Colors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+NC="\033[0m" # No Color
 
-cd /tmp
-rm -f $TARBALL
-rm -rf $DIR
+# Functions
+check_dependencies() {
+    echo -ne "${BLUE}Vérification des dépendances...${NC}"
+    for cmd in "${DEPENDENCIES[@]}"; do
+        command -v "$cmd" >/dev/null 2>&1 || {
+            echo -e "\n${RED}Error: $cmd is not installed. Please install $cmd and try again.${NC}"
+            exit 1
+        }
+    done
+    echo -e "${GREEN} ✔${NC}"
+}
 
-wget $URL/$TARBALL
-if [ $? -ne 0 ]; then
-    echo "Error: wget failed"
-    exit 1
-fi
+download_tarball() {
+    echo -e "${BLUE}Téléchargement de ${TARBALL}...${NC}"
+    rm -f "${TMP_DIR}/${TARBALL}"
+    wget --quiet --show-progress -P "${TMP_DIR}" "${URL}/${TARBALL}"
+}
 
-echo "Extracting $TARBALL"
-tar -xjf $TARBALL
-if [ $? -ne 0 ]; then
-    echo "Error: tar failed"
-    exit 1
-fi
+extract_tarball() {
+    echo -ne "${BLUE}Extraction de ${TARBALL}...${NC}"
+    tar -xjf "${TMP_DIR}/${TARBALL}" -C "${TMP_DIR}"
+    echo -e "${GREEN} ✔${NC}"
+}
 
-echo "Configure"
-cd $DIR
-$SUDO ./configure
-echo "Make install"
-$SUDO make install
-echo "All done!"
+configure_and_install() {
+    echo -e "${BLUE}Configuration...${NC}"
+    (cd "${TMP_DIR}/${DIR}" && ./configure)
+    echo -e "${BLUE}Installation...${NC}"
+    (cd "${TMP_DIR}/${DIR}" && make install)
+}
+
+cleanup() {
+    rm -rf "${TMP_DIR}/${DIR}"
+}
+
+main() {
+    set -e
+    trap cleanup EXIT
+
+    if [ $UID -eq 0 ]; then
+        echo -e "${YELLOW}[no sudo for root]${NC}"
+    else
+        # Add sudo for non-root users
+        SUDO="/usr/bin/sudo"
+    fi
+
+    check_dependencies
+    download_tarball
+    extract_tarball
+    configure_and_install
+
+    echo -e "${GREEN}Fait!${NC}"
+}
+
+main
